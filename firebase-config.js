03786743de6d32d71a9179cee3024ab68e0d1d6d
jotaye <jotayegroupllc@gmail.com>
2025-06-12 23:14:09 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,15 @@
-// Firebase configuración
+// Inicialización Firebase v9 modular
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_PROYECTO.firebaseapp.com",
+  projectId: "TU_PROYECTO",
+  storageBucket: "TU_PROYECTO.appspot.com",
+  messagingSenderId: "TU_SENDER_ID",
+  appId: "TU_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
